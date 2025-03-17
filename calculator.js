40ef9b2ce12e14232dec3b94a62b321a0957d52b
Dane Miyata <48339284+dymiyata@users.noreply.github.com>
2025-03-17 11:15:14 -0700

    Implement new number functionality
    When a new number should be started, make digit press start new number
    as opposed to concatenating to the currently displayed number.

--- calculator.js
+++ calculator.js
@@ -21,11 +21,22 @@ function operate(operation, a, b) {
     return operation(a, b);
 }
 
+let firstArgument = 0;
+let operation = "None"
+let secondArgument = 0;
+let isNewNumber = false;
+
 const displayElem = document.querySelector(".display");
 const numberButtons = document.querySelectorAll(".number");
 
 for (const button of numberButtons) {
     button.addEventListener("click", () => {
-        displayElem.innerHTML += button.value;
+        let stringToDisplay = displayElem.innerHTML;
+        if (isNewNumber) {
+            stringToDisplay = "0";
+            isNewNumber = false;
+        }
+        stringToDisplay = String(Number(stringToDisplay + button.value));
+        displayElem.innerHTML = stringToDisplay;
     })
 }
