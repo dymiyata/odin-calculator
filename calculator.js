336ba170433e5f825d722f5475a847a05d1f4a6a
Dane Miyata <48339284+dymiyata@users.noreply.github.com>
2025-03-17 10:44:51 -0700

    Style buttons and make number buttons functionality

--- calculator.js
+++ calculator.js
@@ -20,3 +20,12 @@ function divide(a, b) {
 function operate(operation, a, b) {
     return operation(a, b);
 }
+
+const displayElem = document.querySelector(".display");
+const numberButtons = document.querySelectorAll(".number");
+
+for (const button of numberButtons) {
+    button.addEventListener("click", () => {
+        displayElem.innerHTML += button.value;
+    })
+}
