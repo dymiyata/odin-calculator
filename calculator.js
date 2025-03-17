ef94086ad16cf85127cb95139d7cc5e85dc8eb80
Dane Miyata <48339284+dymiyata@users.noreply.github.com>
2025-03-17 11:39:14 -0700

    Add functionality to operator buttons

--- calculator.js
+++ calculator.js
@@ -18,7 +18,37 @@ function divide(a, b) {
 }
 
 function operate(operation, a, b) {
-    return operation(a, b);
+    if (operation == "add") {
+        return add(a, b);
+    }
+    if (operation == "multiply") {
+        return multiply(a, b);
+    }
+    if (operation == "subtract") {
+        return subtract(a, b);
+    }
+    if (operation == "divide") {
+        return divide(a, b);
+    }
+}
+
+function doNumberButtonAction(event) {
+    let stringToDisplay = displayElem.innerHTML;
+    if (isNewNumber) {
+        stringToDisplay = "0";
+        isNewNumber = false;
+    }
+    stringToDisplay = String(Number(stringToDisplay + event.target.value));
+    displayElem.innerHTML = stringToDisplay;
+}
+
+function doOperatorButtonAction(event) {
+    if (operation != "None") {
+        displayElem.innerHTML = operate(operation, firstArgument, Number(displayElem.innerHTML));
+    }
+    firstArgument = Number(displayElem.innerHTML);
+    operation = event.target.id;
+    isNewNumber = true;
 }
 
 let firstArgument = 0;
@@ -28,15 +58,12 @@ let isNewNumber = false;
 
 const displayElem = document.querySelector(".display");
 const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+
+for (const button of operatorButtons) {
+    button.addEventListener("click", event => doOperatorButtonAction(event));
+}
 
 for (const button of numberButtons) {
-    button.addEventListener("click", () => {
-        let stringToDisplay = displayElem.innerHTML;
-        if (isNewNumber) {
-            stringToDisplay = "0";
-            isNewNumber = false;
-        }
-        stringToDisplay = String(Number(stringToDisplay + button.value));
-        displayElem.innerHTML = stringToDisplay;
-    })
+    button.addEventListener("click", event => doNumberButtonAction(event));
 }
